a0fa54b570d019e120cafe098a32600790832cd7
miftikharul <miftikharul@gmail.com>
2026-01-23 11:11:11 +0700

    Update projek

---
+++ app/(site)/company-profile/employee-branding/page.jsx
@@ -0,0 +1,12 @@
+import OurClient from "@/components/sections/OurClient";
+
+const { default: Hero } = require("@/components/sections/Hero");
+
+export default function CorporateValuePage() {
+  return (
+    <>
+      <Hero/>
+      <OurClient/>
+      </>
+    );
+}
