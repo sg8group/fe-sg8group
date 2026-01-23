397f402f9054b9d4fb9694841e34320b81d80511
mun4wwar <152259890+mun4wwar@users.noreply.github.com>
2026-01-23 16:27:07 +0700

    fix: fixed responsive layout when mobile view at homePage

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,17 +1,15 @@
 import RecruitmentCTA from "@/components/sections/recruitment-CTA/RecruitmentCTA";
-import Navbar from "../components/layouts/navbar/Navbar";
 import Hero from "../components/sections/Hero";
 import IntroStats from "../components/sections/intro-stats/IntroStats";
 import OurMindset from "../components/sections/OurMindset";
 import OurServices from "../components/sections/service/OurServices";
 import OurClient from "@/components/sections/OurClient";
 
-
 export default function Home() {
   return (
     <>
       <Hero />
-      <main className="flex flex-col gap-16">
+      <main className="flex flex-col gap-10 md:gap-16">
         {/* <h1 className="heading text-4xl text-black">SG8 Group</h1>
         <h2 className="section-title text-black text-2xl">Our Services</h2>
         <p className="text-black">Ini otomatis poppins</p> */}
