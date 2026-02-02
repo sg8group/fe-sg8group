1c88c7053766c42f03e4b83622acb65a2b98af77
miftikharul <miftikharul@gmail.com>
2026-02-02 10:18:21 +0700

    update project

--- app/(site)/services/Consulting/page.jsx
+++ app/(site)/services/Consulting/page.jsx
@@ -7,11 +7,12 @@ import Sectionourservices from "./Sectionourservices";
 import OurServices from "@/components/sections/service/OurServices";
 import OurClient from "@/components/sections/OurClient";
 import Sectionchoose from "./Sectionchoose";
+import Hero from "@/components/sections/Hero";
 
 export default function ConsultingPage() {
   return (
     <main className="bg-white">
-      <Consultinghero />
+      <Hero/>
       <Consultingintro />
       <TailoredSolutions />
       <Sectionourservices />
